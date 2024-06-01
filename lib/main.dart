f5b762e7f353e20b5f5b5a35b19f07867737bae8
Sujit <sujitthapamagar471@gmail.com>
2024-06-01 15:18:42 +0545

    splash screen animation added

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'homepage/homepage.dart';
 import 'navigation_menu/navigation_bar.dart';
-
+import 'package:lottie/lottie.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -29,7 +30,31 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: NavigationMenu(),
+      home:SplashScreen(),
+    );
+  }
+}
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedSplashScreen(
+        splash:SizedBox.expand(
+          child: Lottie.asset('animation/netflix.json',
+            repeat: true,
+            reverse: true,
+            animate: true,
+            fit: BoxFit.cover,
+          ),
+        ),
+        nextScreen: NavigationMenu(),
+        // nextScreen: NavigationMenu(),
+        splashTransition: SplashTransition.fadeTransition,
+        duration: 3000,
+        backgroundColor: Colors.black,
+      ),
     );
   }
 }
